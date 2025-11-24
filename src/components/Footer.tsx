32726c02b05345fd4c44e816279fac4ffb5ddf9a
theChapMan <gchapm06@uoguelph.ca>
2025-11-24 15:26:27 -0500

    Switch footer logo back to img tag with explicit dimensions

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default function Footer() {
   return (
@@ -9,13 +8,13 @@ export default function Footer() {
           {/* Company Info */}
           <div className="col-span-1 md:col-span-2">
             <Link href="/" className="inline-block mb-4">
-              <Image
+              <img
                 src="/images/chapman-digital-logo-white.png"
                 alt="Chapman Digital Services"
                 width={200}
                 height={56}
                 className="h-14 w-auto"
-                priority
+                style={{ display: 'block' }}
               />
             </Link>
             <p className="text-neutral-300 mb-4 max-w-md">
